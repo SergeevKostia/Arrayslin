c145c2296e831e08adda490f0d00e9129b07726a
Konstantin Sergeev <sergeeff.kossta@gmail.com>
2023-05-10 12:47:07 +0300

    main

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,59 @@
 package ru.netology.stats;
 
 public class StatsService {
+    public int getAmount(int[] sales) {
+        int amount = 0;
+
+        for (int i : sales) {
+            amount += i;
+        }
+        return amount;
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int getAverage(int[] sales) {
+        int mid = getAmount(sales) / sales.length;
+        return mid;
+    }
+
+    public int underAverage(int[] sales) {
+        int undAverage = 0;
+        for (int sale : sales) {
+            if (sale < getAverage(sales)) {
+                undAverage += 1;
+            }
+        }
+        return undAverage;
+    }
+
+    public int overAverage(int[] sales) {
+        int ovrAverage = 0;
+        for (int sale : sales) {
+            if (sale > getAverage(sales)) {
+                ovrAverage += 1;
+            }
+        }
+        return ovrAverage;
+    }
+
 }
---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,70 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatsService;
+
+public class StatsServiceTest {
+    @Test
+    public void shouldFindAmountSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 180;
+        int actual = service.getAmount(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxMonth() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMonth = 8;
+        int actualMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+
+    }
+
+    @Test
+    public void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMonth = 9;
+        int actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+
+    }
+
+    @Test
+    public void shouldFindUnderAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actual = service.underAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOverAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actual = service.overAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
